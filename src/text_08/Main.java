7285ba02291573db475821629ae526a089b98c27
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-08-29 11:45:29 +0900

    JDBC에 article write 저장하기

--- src/text_08/Main.java
+++ src/text_08/Main.java
@@ -1,5 +1,9 @@
 package text_08;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -37,6 +41,50 @@ public class Main {
 				articles.add(article);
 				lastArticleId++;
 				System.out.println(article);
+				
+				Connection conn = null;
+				PreparedStatement pstat = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = \""+ title +"\"";
+					sql += ", `body` = \""+ body +"\";";
+
+//					System.out.println(sql);
+
+					pstat = conn.prepareStatement(sql);
+					int affectedRows = pstat.executeUpdate();
+
+					System.out.println("affectedRows : " + affectedRows);
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 			if (command.equals("article list")) {
 
