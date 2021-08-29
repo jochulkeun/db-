f8f5342775e45ae2e61e9e05deef4355ace51664
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-08-29 12:41:01 +0900

    SELECT TEST

--- src/text_08/Article.java
+++ src/text_08/Article.java
@@ -4,14 +4,26 @@ public class Article {
 	int id;
 	String title;
 	String body;
-	public Article(int id, String body, String title) {
+	String regDate;
+	String updateDate;
+	public Article(int id, String title, String body) {
 		this.id = id;
-		this.body = body;
 		this.title = title;
+		this.body = body;
+	}
+	public Article(int id,String title,String body, String regDate, String updateDate) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
 	}
 	@Override
 	public String toString() {
-		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + ", regDate=" + regDate + ", updateDate="
+				+ updateDate + "]";
 	}
+	
+	
 
 }
