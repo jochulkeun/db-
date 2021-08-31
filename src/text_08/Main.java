8f61a16e6167a207b2377695c2aaf9569c73d3d7
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-08-31 20:40:32 +0900

    게시글 수정

--- src/text_08/Main.java
+++ src/text_08/Main.java
@@ -86,6 +86,64 @@ public class Main {
 					}
 				}
 			}
+			if (command.startsWith("article modify ")) {
+
+				int id = Integer.parseInt(command.split(" ")[2]);
+				
+				System.out.printf("==%d번 게시글 수정==\n",id);
+				System.out.printf(" 새 제목 :");
+				String title = sc.nextLine();
+				System.out.printf("내용 :");
+				String body = sc.nextLine();
+
+				System.out.printf("%d 번글이 수정되었습니다.\n", id);
+				Article article = new Article(id, body, title);
+				lastArticleId++;
+				System.out.println(article);
+				
+				Connection conn = null;
+				PreparedStatement pstat = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "UPDATE article";
+					sql += " SET updateDate = NOW()";
+					sql += ", title = \""+ title +"\"";
+					sql += ", `body` = \""+ body +"\"";
+					sql += "WHERE id = " +id;
+//					System.out.println(sql);
+
+					pstat = conn.prepareStatement(sql);
+					int affectedRows = pstat.executeUpdate();
+
+					System.out.println("affectedRows : " + affectedRows);
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 			if (command.equals("article list")) {
 				
 				Connection conn = null;
