380620744d5c32894356292ee821fe6658f4077b
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-09-06 20:53:54 +0900

    아이디 중복체크

--- src/text_08/App.java
+++ src/text_08/App.java
@@ -62,80 +62,89 @@ public class App {
 
 	private int doAction(Connection conn, Scanner scanner, String cmd) {
 		if (cmd.equals("member join")) {
-
 			String loginId;
 			String loginPw;
 			String loginPwConfirm;
 			String name;
-			
-			System.out.println("== 회원가입 ==");
-
 			while (true) {
-				System.out.printf("로그인아이디 : ");
-				loginId = scanner.nextLine().trim();
+				System.out.println("== 회원가입 ==");
+				System.out.printf("로그인 아이디 : ");
+				loginId = scanner.nextLine();
 
 				if (loginId.length() == 0) {
-					System.out.println("로그인 아이디를 입력해 주세요\n");
+					System.out.println("로그인 아이디를 확인하세요");
+					continue;
+				}
+				SecSql sql = new SecSql();
+				sql.append("SELECT COUNT(*) > 0");
+				sql.append("FROM member");
+				sql.append("WHERE loginId = ?", loginId);
+				
+				boolean isloginIdDup = DBUtil.selectRowBooleanValue(conn, sql);
+				
+				if (isloginIdDup) {
+					System.out.printf("%s는 사용중인 아이디 입니다.\n",loginId);
 					continue;
 				}
 				break;
 			}
-
+			
 			while (true) {
 
-				System.out.printf("로그인비번 : ");
+				System.out.printf("로그인 비밀번호 : ");
 				loginPw = scanner.nextLine().trim();
 
 				if (loginPw.length() == 0) {
-					System.out.println("로그인 비번을 입력해 주세요\n");
+					System.out.println("로그인 비밀번호를 확인하세요");
 					continue;
 				}
 				boolean loginPwConfirmIsSame = true;
-
 				while (true) {
-					System.out.printf("로그인비번 확인 : ");
-					loginPwConfirm = scanner.nextLine().trim();
-
-					if (loginPw.length() == 0) {
-						System.out.println("로그인 비번확인해주세요\n");
+					
+					System.out.printf("로그인 비번확인 : ");
+					loginPwConfirm = scanner.nextLine();
+					
+					if (loginPwConfirm.length() == 0) {
+						System.out.println("로그인 비밀번호를 확인하세요");
 						continue;
-
 					}
 					if (loginPw.equals(loginPwConfirm) == false) {
-						System.out.println("로그인 비번이 일치하지않습니다.\n");
+						System.out.println("로그인 비밀번호를 확인하세요");
 						loginPwConfirmIsSame = false;
 						break;
+				
 					}
 					break;
 				}
-
-				if (loginPwConfirmIsSame) {
-
+				if(loginPwConfirmIsSame) {
 					break;
 				}
+			
 			}
-			while (true) {
-				System.out.printf("이름 : ");
+			while(true) {
+				System.out.print("이름 :");
 				name = scanner.nextLine().trim();
-
-				if (name.length() == 0) {
-					System.out.println("이름을 입력해 주세요\n");
+				if(name.length() == 0) {
+					System.out.printf("이름을 입력해 주세요 : ");
 					continue;
 				}
-				break;
+			break;
 			}
 			SecSql sql = new SecSql();
 
 			sql.append("INSERT INTO member");
-			sql.append("SET regDate = NOW()");
+			sql.append(" SET regDate = NOW()");
 			sql.append(", updateDate = NOW()");
 			sql.append(", loginId = ?", loginId);
 			sql.append(", loginPw = ?", loginPw);
 			sql.append(", `name` = ?", name);
+			
 
 			int id = DBUtil.insert(conn, sql);
-			System.out.printf("%s님 환영합니다.\n",name);
+
+			System.out.printf("%s님 환영합니다.\n", name);
 		}
+
 		if (cmd.equals("article write")) {
 
 			String title;
